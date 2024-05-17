f96187e8a7bba4f2f44a20faeb5c12d5cd01bff2
Tam Nguyen <143235252+tamknguyen3@users.noreply.github.com>
2024-05-17 17:54:13 -0400

    reviewing the basics of python

---
+++ pythonbasics.py
@@ -0,0 +1,26 @@
+# a // b divides a by b and returns the integer part
+# string concatenation
+# str() converts non-string into a string
+# len() return length of a string 
+# .upper() and .lower() built-in methods
+# indexing string[number] returns one char 
+# slicing string[:4] returns a chunk of chars
+# .replace(string, string) method replaces substrings within a string with another substring
+# .strip() method removes (by default) the whitespace at the beginning or end of a string
+
+# check if substring is in a string -- in keyword -- stored as a boolean value 
+string = "I am a Python programmer!"
+substring = "Python"
+Python_in_string = substring in string # -- not in -- basically does the opposite 
+print(Python_in_string)
+
+# int() and float() functions converts value into their data types respectively
+# scientific numbers e or E meaning -- times 10 to the power
+# complex numbers -- real and imagine part -- in python using j instead of i 
+# incrementing += and decrementing -=      applies to other operators too: **=, //=
+# Boolean comparisons == != > < >- <=
+# Boolean operators and or not 
+print("\n\n\n")
+
+# basic online purchasing system 
+print("Welcome to the online shopping experience!")
