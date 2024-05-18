dbab606d48c7c3de0876b56ac9d443f3d2040ad9
Tam Nguyen <143235252+tamknguyen3@users.noreply.github.com>
2024-05-17 23:57:00 -0400

    singlylinked list -- implemented and tested append; updated README

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 arrays
 - merging arrays
 - binary search (two pointer approach)
+
+linked list
+- linear data structure where each element (node) is a separate object made of at least two items
+-- the data and a reference to the next element 
--- singlylinkedlist.py
+++ singlylinkedlist.py
@@ -10,9 +10,9 @@ class SinglyLinkedList:
 
   # method that adds to the front of the list
   def prepend(self, data):
-    new_node = Node(data) # create new node 
-    new_node.next = self.head # new node points to current head
-    self.head = new_node # update head
+    new_node = Node(data)               # create new node 
+    new_node.next = self.head           # new node points to current head
+    self.head = new_node                # update head
 
   # method that iterates over a linked list and
   # prints the data stored at every node
@@ -35,6 +35,21 @@ class SinglyLinkedList:
       current = current.next
       count += 1
     raise IndexError("Index out of bounds")
+  
+  # method that creates a node and adds it to the back of the list
+  def append(self, data):
+    new_node = Node(data)         # create new node
+    if self.head is None:         # edge case, there's no current node in list to append to 
+      self.head = new_node
+      return
+    
+    last = self.head              # traverse tail pointer to reference its next to the new node 
+    while last.next:
+      last = last.next 
+    last.next = new_node
+
+
+
 
 if __name__ == "__main__":
   sll = SinglyLinkedList()
@@ -42,16 +57,22 @@ if __name__ == "__main__":
   sll.prepend(1)
   sll.prepend(2)
   sll.prepend(3)
-
   print("Linked List after prepending:")
   sll.traverse()
+
+  
+  sll.append(40)
+  sll.append(50)
+  print("Linked list after appending:")
+  sll.traverse()  
 
   try:
     print("Element at index 0:", sll.get(0))
     print("Element at index 1:", sll.get(1))
     print("Element at index 2:", sll.get(2))
     print("Element at index 3:", sll.get(3))
-
+    print("Element at index 4:", sll.get(4))  
+    print("Element at index 5:", sll.get(5))  # expected to raise IndexError
 
   except IndexError as e:
     print(e) # Index out of bounds 
